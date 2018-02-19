dd4d3fd4659858f0004fa257554b2dd93a801fd5
Rafal Molenda <Rafal.Molenda@smart4aviation.aero>
2018-02-19 11:27:36 +0100

    Initializing the project with spring-boot 1.5.10 with lombock project included

---
+++ src/main/java/pl/rafalmol/recruitment/pricecomparison/PriceComparisonApplication.java
@@ -0,0 +1,12 @@
+package pl.rafalmol.recruitment.pricecomparison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceComparisonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceComparisonApplication.class, args);
+	}
+}
